eec665571c5eddf2cb34b85e290fefc471d4b2fd
Charles Pascoe <cpascoe95@gmail.com>
2017-01-23 14:42:09 +0000

    Add GET /token route

--- src/server/routers.js
+++ src/server/routers.js
@@ -1,10 +1,27 @@
 import { Router } from 'express';
 import authenticator from 'server/authenticator';
 import loggers from 'server/loggers';
+import catchAsync from 'server/catch-async';
+
+const catchHandler = catchAsync((err, req, res) => {
+  loggers.main.error({err: err});
+  res.status(500).end();
+});
 
 const router = new Router();
 
 router.use(authenticator.authenticate);
+
+router.get('/token', catchHandler(async function (req, res) {
+  if (req.singleUseToken) {
+    res.status(403).end();
+    return;
+  }
+
+  var token = await authenticator.createSingleUseToken();
+
+  res.status(200).send(token + '\n');
+}));
 
 router.post('/bin', function (req, res) {
   loggers.main.debug(req.body);
