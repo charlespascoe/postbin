c0fe36a93bb44ad9f396a3366c8ac8bf2df7181d
Charles Pascoe <cpascoe95@gmail.com>
2017-01-23 14:41:24 +0000

    Add Utils.randomBytes method

--- src/server/utils.js
+++ src/server/utils.js
@@ -1,3 +1,5 @@
+import crypto from 'crypto';
+
 export default class Utils {
   static defaults(defaults, additional) {
     return Object.assign(Utils.copy(defaults), additional);
@@ -5,5 +7,18 @@ export default class Utils {
 
   static copy(obj) {
     return Object.assign({}, obj);
+  }
+
+  static randomBytes(length) {
+    return new Promise((fulfill, reject) => {
+      crypto.randomBytes(length, (err, buff) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+
+        fulfill(buff);
+      });
+    });
   }
 }
