23554d9cc31da69ca68daadbcd9e025241470d6a
Charles Pascoe <cpascoe95@gmail.com>
2017-01-24 12:01:07 +0000

    Implement posting/getting data

--- src/server/routers.js
+++ src/server/routers.js
@@ -2,6 +2,9 @@ import { Router } from 'express';
 import authenticator from 'server/authenticator';
 import loggers from 'server/loggers';
 import catchAsync from 'server/catch-async';
+import config from 'server/config';
+import path from 'path';
+import afs from 'server/afs';
 
 const catchHandler = catchAsync((err, req, res) => {
   loggers.main.error({err: err});
@@ -18,15 +21,44 @@ router.get('/token', catchHandler(async function (req, res) {
     return;
   }
 
-  var token = await authenticator.createSingleUseToken();
+  let token = await authenticator.createSingleUseToken();
 
   res.status(200).send(token + '\n');
 }));
 
-router.post('/bin', function (req, res) {
-  loggers.main.debug(req.body);
-  res.status(201).end();
-});
+router.post('/bin', catchHandler(async function (req, res) {
+  let id = Date.now().toString(),
+      filePath = path.join(config.dataDir, id);
+
+  await afs.writeFile(filePath, req.body);
+
+  res.status(201).send(id + '\n');
+}));
+
+router.get('/bin/:id', catchHandler(async function (req, res) {
+  if (!/^\d+$/.test(req.params.id)) {
+    res.status(400).end();
+    return;
+  }
+
+  let content;
+  try {
+    content = await afs.readFile(path.join(config.dataDir, req.params.id));
+  } catch (err) {
+    if (err.code == 'ENOENT') {
+      res.status(404).end();
+      return;
+    }
+
+    throw err;
+  }
+
+  if (!req.query.nonewline) {
+    content += '\n';
+  }
+
+  res.status(200).send(content);
+}));
 
 export default function (app) {
   app.use('/', router);
